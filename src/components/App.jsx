f806cce6144edd8563d4aa0aea4a7ca71ad0f505
unknown <al_jager1983@yahoo.com>
2022-10-23 07:27:07 +0800

    Keeper App exercise part2, with arrow functions and mapping

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,12 +2,22 @@ import React from 'react'
 import Header from './Header'
 import Footer from './Footer'
 import Note from './Note'
+import Notes from '../Notes'
+
+const createNote = (cNote) => {
+    return (
+        <Note   
+                key={cNote.key}
+                title={cNote.title}
+                content={cNote.content} />
+    )
+}
 
 const App = () => {
     return (
     <div>
         <Header />
-        <Note />
+        {Notes.map(createNote)}
         <Footer />
         
     </div>
--- src/index.js
+++ src/index.js
@@ -12,7 +12,7 @@
 //HINT: You will need to study the classes in teh styles.css file to appy styling.
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App'
+import App from './components/App'
 
 const root = ReactDOM.createRoot(document.getElementById('root'))
 root.render (
